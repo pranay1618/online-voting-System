86470ba74fe681e88a4bd9b8a59b8f5ae0b61983
pranay1618 <113667638+pranay1618@users.noreply.github.com>
2024-02-20 17:37:07 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+    MONGO_URI:"mongodb+srv://mahen:12345a@cluster0.qfgbe.mongodb.net/E-voting?retryWrites=true&w=majority",
+    // MONGO_URI:"mongodb://127.0.0.1/ov2",
+    JWT_SECRET:"MOHENMONDALJDFKFLL"
+
+}
